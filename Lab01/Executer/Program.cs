91824b15bab4e8c9562892e5fcd6f0ad350fc79b
Ivan Capponi <ivan.capponi@outlook.com>
2017-10-18 21:42:11 +0200

    First commit

---
+++ Lab01/Executer/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Reflection;
+using MyAttribute;
+
+namespace Executer
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Assembly assembly = Assembly.LoadFrom("MyLibrary.dll");
+            foreach (Type type in assembly.GetTypes())
+                if (type.IsClass)
+                {
+                    MethodInfo[] methods = type.GetMethods();
+                    foreach (MethodInfo singleMethod in methods)
+                    {
+                        object classInstance = Activator.CreateInstance(type, null);
+                        ParameterInfo[] parameters = singleMethod.GetParameters();
+
+                        if (parameters.Length == 0)
+                            singleMethod.Invoke(classInstance, null);
+                        else
+                        {
+                            ExecuteMeAttribute[] attr = (ExecuteMeAttribute[]) singleMethod.GetCustomAttributes(typeof(ExecuteMeAttribute), true);
+                            foreach (ExecuteMeAttribute single in attr)
+                                singleMethod.Invoke(classInstance, single.ArgsObjects);    
+                        }   
+                    }
+                }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Lab01/MyAttribute/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace MyAttribute
+{
+    [AttributeUsage(AttributeTargets.Method, AllowMultiple = true)]
+    public class ExecuteMeAttribute : Attribute
+    {
+        public object[] ArgsObjects { get; }
+
+        public ExecuteMeAttribute(params Object[] argsObjects)
+        {
+            ArgsObjects = argsObjects;
+        }
+    }
+}
