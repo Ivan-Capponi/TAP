47cc99f558511c07875f4b884b1a0ee3f7dc5db3
Ivan Capponi <ivan.capponi@outlook.com>
2017-10-20 00:45:13 +0200

    Refactoring fix

--- Lab01/Executer/Executer.cs
+++ Lab01/Executer/Executer.cs
@@ -12,7 +12,7 @@ namespace Executer
             foreach (Type type in assembly.GetTypes())
                 if (type.IsClass)
                 {
-                    if (IsDefaultConstructor(type.GetConstructors()))
+                    if (HasDefaultConstructor(type.GetConstructors()))
                         DefaultConstructor(type);
                     else
                         NonDefaultConstructor(type);
@@ -21,57 +21,49 @@ namespace Executer
             Console.ReadLine();
         }
 
+        private static void DefaultConstructor(Type type)
+        {
+            object classInstance;
+            try { classInstance = Activator.CreateInstance(type, null); }
+            catch (MissingMethodException) { return; }
+            InvokeMethods(type, classInstance);
+        }
+
         private static void NonDefaultConstructor(Type type)
         {
             ConstructorInfo[] constructors = type.GetConstructors();
             foreach (ConstructorInfo singleConstructor in constructors)
             {
-                NonDefaultConstructorAttribute[] attributes = (NonDefaultConstructorAttribute[]) singleConstructor.GetCustomAttributes(typeof(NonDefaultConstructorAttribute), true);
+                NonDefaultConstructorAttribute[] attributes = (NonDefaultConstructorAttribute[])singleConstructor.GetCustomAttributes(typeof(NonDefaultConstructorAttribute), true);
                 foreach (NonDefaultConstructorAttribute singleAttribute in attributes)
                 {
                     object classInstance;
                     try { classInstance = Activator.CreateInstance(type, singleAttribute.ArgsObjects); }
-                    catch (MissingMethodException){ return; }
-                    foreach (MethodInfo singleMethod in type.GetMethods())
-                    {
-                        ParameterInfo[] parameters = singleMethod.GetParameters();
-                        if (parameters.Length == 0)
-                           try { singleMethod.Invoke(classInstance, null); }
-                           catch (ArgumentException) { Console.WriteLine("Failed: {0}", singleMethod.Name); }
-                        else
-                        {
-                            ExecuteMeAttribute[] attr = (ExecuteMeAttribute[]) singleMethod.GetCustomAttributes(typeof(ExecuteMeAttribute), true);
-                            foreach (ExecuteMeAttribute single in attr)
-                                try { singleMethod.Invoke(classInstance, single.ArgsObjects); }
-                                catch (ArgumentException){ Console.WriteLine("Failed: {0}", singleMethod.Name); }
-                        }
-                    }
+                    catch (MissingMethodException) { return; }
+                    InvokeMethods(type, classInstance);
                 }
             }
         }
 
-        private static void DefaultConstructor(Type type)
+        private static void InvokeMethods(Type type, object classInstance)
         {
-            MethodInfo[] methods = type.GetMethods();
-            object classInstance;
-            try { classInstance = Activator.CreateInstance(type, null); }
-            catch (MissingMethodException) { return; }
-            foreach (MethodInfo singleMethod in methods)
+            foreach (MethodInfo singleMethod in type.GetMethods())
             {
                 ParameterInfo[] parameters = singleMethod.GetParameters();
                 if (parameters.Length == 0)
-                    try {singleMethod.Invoke(classInstance, null);}
+                    try { singleMethod.Invoke(classInstance, null); }
                     catch (ArgumentException) { Console.WriteLine("Failed: {0}", singleMethod.Name); }
                 else
                 {
                     ExecuteMeAttribute[] attr = (ExecuteMeAttribute[])singleMethod.GetCustomAttributes(typeof(ExecuteMeAttribute), true);
                     foreach (ExecuteMeAttribute single in attr)
-                        try { singleMethod.Invoke(classInstance, single.ArgsObjects); } catch (ArgumentException) { Console.WriteLine("Failed: {0}", singleMethod.Name); }
+                        try { singleMethod.Invoke(classInstance, single.ArgsObjects); }
+                        catch (ArgumentException) { Console.WriteLine("Failed: {0}", singleMethod.Name); }
                 }
             }
         }
 
-        private static bool IsDefaultConstructor(ConstructorInfo[] constructors)
+        private static bool HasDefaultConstructor(ConstructorInfo[] constructors)
         {
             Boolean defaultFlag = false;
             foreach (ConstructorInfo singleConstructor in constructors)
