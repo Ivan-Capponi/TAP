4a37b7dc6ba9718e92e6ad35cf2e56924544fda3
Ivan Capponi <ivan.capponi@outlook.com>
2017-12-20 15:18:26 +0100

    Added login system to LoadBugTracking

--- Lab06/BugTrackingSystem/BTS.cs
+++ Lab06/BugTrackingSystem/BTS.cs
@@ -1,47 +1,71 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Data.Common;
 using System.Data.Entity;
 using System.Data.SqlClient;
 using System.Diagnostics;
+using System.Linq;
 using Domain;
 
 namespace BugTrackingSystem
 {
     public class BugTrackingSystem
     {
-
+        private static SystemContext _context;
+        public static void AddNewUser(String username, String adminPassword)
+        {
+            Utente utente = new Utente();
+            utente.Login = username;
+            utente.Password = adminPassword;
+            utente.Date = DateTime.Today;
+            _context.Utenti.Add(utente);
+            _context.SaveChanges();
+            _context.Dispose();
+        }
         public static void InitializeBugTracking(string connectionString, string adminPassword)
         {
-            SystemContext s = new SystemContext();
-            s.Database.Connection.ConnectionString = connectionString;
-            Database.SetInitializer(new DropCreateDatabaseAlways<SystemContext>());
-            s.Database.Initialize(true);
-            Utente admin = new Utente();
-            admin.Login = "Admin";
-            admin.Password = adminPassword;
-            admin.Date = DateTime.Today;
-            s.Utenti.Add(admin);
-            s.SaveChanges();
-            s.Dispose();
+            _context = new SystemContext();
+            _context.Database.Connection.ConnectionString = connectionString;
+            try
+            {
+                Database.SetInitializer(new DropCreateDatabaseAlways<SystemContext>());
+                _context.Database.Initialize(true);
+            }
+            catch (SqlException e)
+            {
+                Console.WriteLine("La base di dati è attualmente occupata. Il programma terminerà");
+                Debug.WriteLine(e.Message);
+                _context.Dispose();
+                throw;
+            }
+            
+            AddNewUser("admin", adminPassword);
         }
 
         public static void LoadBugTracking(string connectionString, string adminPassword)
         {
-            SystemContext s = new SystemContext();
-            s.Database.Connection.ConnectionString = connectionString;
+            _context = new SystemContext();
+            _context.Database.Connection.ConnectionString = connectionString;
             try
             {
-                DbConnection conn = s.Database.Connection;
+                DbConnection conn = _context.Database.Connection;
                 conn.Open();
-            }
+                List <string> login = (from utenti in _context.Utenti where utenti.Password == adminPassword select utenti.Nome).ToList();
+                if (login.Count == 1)
+                    Console.WriteLine("Login eseguito correttamente");
+                else
+                {
+                    Console.WriteLine("Login fallito");
+                }
+             }
             catch (SqlException e)
             {
                 Console.WriteLine("Impossibile connettersi al database. Il programma terminerà.");
                 Debug.WriteLine(e.Message);
-                s.Dispose();
+                _context.Dispose();
                 throw;
             }
-            s.Dispose();
+            _context.Dispose();
         }
     }
 }
--- Lab06/TestApplication/Program.cs
+++ Lab06/TestApplication/Program.cs
@@ -6,7 +6,7 @@ namespace TestApplication
     {
         static void Main()
         {
-            BugTrackingSystem.BugTrackingSystem.LoadBugTracking("Server=.\\SQLEXPRESS;Database=TAP3;Integrated Security=True;", "TAP123");
+            BugTrackingSystem.BugTrackingSystem.LoadBugTracking("Server=.\\SQLEXPRESS;Database=TAP;Integrated Security=True;", "TAP123");
             Console.WriteLine("OK");
             Console.ReadLine();
         }
